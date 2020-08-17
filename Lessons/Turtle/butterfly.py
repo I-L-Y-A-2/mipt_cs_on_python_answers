29f6a9540c4a730d684a7a56a92774df4e045c57
Ilya Verbilo <verbiloilyaa@gmail.com>
2020-08-17 22:58:08 +0300

    11&12 tasks

--- Lessons/Turtle/butterfly.py
+++ Lessons/Turtle/butterfly.py
@@ -1,26 +1,32 @@
 import turtle
+import math
+
 
 turtle.shape('turtle')
-flowers = 3
 
 
-def flower():
+def flower(r):
     """
     Функция рисует две симметричные окружности
     :return:
     """
+    c = 2 * math.pi * r
+    len_side = c/180
     sides = 0
     while sides != 361:
         if sides <= 180:
-            turtle.forward(3)
+            turtle.forward(len_side)
             turtle.left(2)
         elif sides > 180:
-            turtle.forward(3)
+            turtle.forward(len_side)
             turtle.right(2)
         sides += 1
 
 
-while flowers != 0:
-    flower()
-    turtle.left(60)
-    flowers -= 1
+circles = 0
+r = 20
+turtle.left(90)
+while circles != 10:
+    flower(r)
+    r += 10
+    circles += 1
---
+++ Lessons/Turtle/spring.py
@@ -0,0 +1,14 @@
+import turtle as t
+import math
+
+t.shape('turtle')
+t.left(90)
+sides = 0
+while sides != 60:
+    t.forward(3)
+    t.right(3)
+    sides += 1
+
+sides_1 = 0
+while sides_1 != 60:
+    
