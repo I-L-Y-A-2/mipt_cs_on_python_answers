ed2d21cda64bf7988d20be05763f993d1ccdd054
Ilya Verbilo <verbiloilyaa@gmail.com>
2020-10-01 13:33:03 +0300

    stars modifed

---
+++ Lessons/Turtle/stars.py
@@ -0,0 +1,10 @@
+n = int(input("Введите количество чисел: "))
+a = [True] * n
+a[0] = a[1] = False
+for k in range(2, n):
+    if a[k]:
+        for m in range(2*k, n, k):
+            a[m] = False
+
+for k in range(2, n):
+    print(k, '-', "простое" if a[k] else "составное")
